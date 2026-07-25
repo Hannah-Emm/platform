17256004aeed16257d4801c7e09d3a4aa212dbb8
Hannah Molineaux <hannah.molineaux@gmail.com>
2026-07-25 12:31:12 +0100

    Freeze player on impact with person and add rage after player is unfrozen

--- player.gd
+++ player.gd
@@ -12,9 +12,15 @@ signal hit()
 @export var max_rage_level: float = 1000.0
 @export var dash_cost: float = 100.0
 @export var jump_cost: float = 25.0
+@export var unstuck_rage_bonus: float = 100.0
+
+@onready var dash_timer: Timer = $DashTimer
+@onready var freeze_timer: Timer = $FreezeTimer
+
 var current_rage_level: float = 0.0
 var current_speed: float = 0
 var is_dashing: bool = false
+var is_frozen: bool = false
 
 
 @onready var camera: Camera3D = $Camera3D
@@ -22,37 +28,33 @@ var mouse_motion: Vector2 = Vector2()
 
 func _process(delta: float) -> void:
 	if velocity.x == 0 and velocity.z == 0:
-		current_rage_level = min(current_rage_level + (10 * delta), max_rage_level)
+		add_rage(10 * delta)
 	else:
-		current_rage_level = min(current_rage_level + (1 * delta), max_rage_level)
-	rage_level.emit(current_rage_level / max_rage_level)
+		add_rage(1 * delta)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		# Handle jump.
 		if Input.is_action_just_pressed("move_jump") and is_on_floor() and use_rage(jump_cost):
 			velocity.y = jump_velocity
-
-		# Get the input direction and handle the movement/deceleration.
-		var input_dir := Input.get_vector("move_left", "move_right", "move_foward", "move_backward")
-		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		if direction and !is_dashing:
-			if Input.is_action_just_pressed("move_dash") and use_rage(dash_cost):
-				$DashTimer.start()
-				is_dashing = true
-				current_speed = dash_speed
-			elif $DashTimer.is_stopped():
-				current_speed = min(current_speed + acceleration, max_speed)
-			velocity.x = direction.x * current_speed
-			velocity.z = direction.z * current_speed
-		elif !is_dashing:
-			current_speed = max(current_speed - deceleration, 0)
-			velocity.x = move_toward(velocity.x, 0, deceleration)
-			velocity.z = move_toward(velocity.z, 0, deceleration)
+		if !is_frozen:
+			var input_dir := Input.get_vector("move_left", "move_right", "move_foward", "move_backward")
+			var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+			if direction and !is_dashing:
+				if Input.is_action_just_pressed("move_dash") and use_rage(dash_cost):
+					dash_timer.start()
+					is_dashing = true
+					current_speed = dash_speed
+				else:
+					current_speed = min(current_speed + acceleration, max_speed)
+				velocity.x = direction.x * current_speed
+				velocity.z = direction.z * current_speed
+			elif !is_dashing:
+				current_speed = max(current_speed - deceleration, 0)
+				velocity.x = move_toward(velocity.x, 0, deceleration)
+				velocity.z = move_toward(velocity.z, 0, deceleration)
 
 	move_and_slide()
 
@@ -68,6 +70,10 @@ func _input(event: InputEvent) -> void:
 		camera.rotate_x(-event.relative.y * mouse_sensitivity)
 		camera.rotation.x = clampf(camera.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 
+func add_rage(amount: float) -> void:
+	current_rage_level = min(current_rage_level + amount, max_rage_level)
+	rage_level.emit(current_rage_level / max_rage_level)
+
 func use_rage(cost: float) -> bool:
 	if current_rage_level >= cost:
 		current_rage_level -= cost
@@ -76,9 +82,14 @@ func use_rage(cost: float) -> bool:
 	return false
 
 func hit_person():
-	hit.emit()
 	current_speed = 0
+	velocity.x = 0
+	velocity.z = 0
 	is_dashing = false
+	if freeze_timer.is_stopped() or !is_frozen:
+		freeze_timer.start()
+		hit.emit()
+		is_frozen = true
 
 func _on_hit_box_body_shape_entered(_body_rid: RID, body: Node3D, _body_shape_index: int, _local_shape_index: int) -> void:
 	if body is CharacterBody3D and body != self:
@@ -87,3 +98,7 @@ func _on_hit_box_body_shape_entered(_body_rid: RID, body: Node3D, _body_shape_in
 func _on_dash_timer_timeout() -> void:
 	current_speed = max_speed
 	is_dashing = false
+
+func _on_freeze_timer_timeout() -> void:
+	add_rage(unstuck_rage_bonus)
+	is_frozen = false
